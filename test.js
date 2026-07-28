83b020229e1a55de72cedab560334a036c1ce05c
raet0 <yosoyrafaelprieto@gmail.com>
2026-07-28 14:23:48 -0500

    fix

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 
 function runTest() {
-  assert.strictEqual(1 + 1, 3, 'Math works');
+  assert.strictEqual(1 + 1, 2, 'Math works');
   console.log('Tests passed!');
 }
 
